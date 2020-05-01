4636abf63c4e010de757956f1f84eaf5d5f52f96
MrTrustworthy <tinywritingmakesmailhardtoread@gmail.com>
2020-05-01 12:10:04 +0200

    switched to postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,21 +12,26 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
+def env(key):
+    value = os.getenv(key, "")
+    assert value, f"No value set for {key}"
+    return value
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!d8hha)j)9j8nj-e%zx33r8#8t#_32gz@h$y=1zb3s=lp57je$'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,17 +76,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': env("PG_USER"),
+        'PASSWORD': env("PG_PASSWORD"),
+        'HOST': env("PG_HOST"),
+        'PORT': env("PG_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -101,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -114,7 +120,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
