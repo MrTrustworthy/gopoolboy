be916fdcbe0fa55a00c169203f5d561c0df844f4
MrTrustworthy <tinywritingmakesmailhardtoread@gmail.com>
2020-05-03 16:26:21 +0200

    working detail page

--- frontend/src/components/QuestionDetail.vue
+++ frontend/src/components/QuestionDetail.vue
@@ -2,37 +2,22 @@
     <div class="question-overview">
         <h1>Question {{ $route.params.id }}</h1>
 
-        <ApolloQuery
-                :query="require('../graphql/QuestionDetail.gql')"
-                :variables="{ question_id }"
+
+        <div v-if="$apollo.queries.question.loading" class="loading apollo">Loading...</div>
+
+        <div class="question-box">
+            <h3>{{ question.title }}</h3>
+            <p>{{ question.text }}</p>
+        </div>
+        <br>
+
+        <div
+                v-for="answer of question.answerSet"
+                :key="answer.id"
+                class="answer-box"
         >
-            <template slot-scope="{ result: { loading, error, data } }">
-                <div v-if="loading" class="loading apollo">Loading...</div>
-
-                <!-- Error -->
-                <div v-else-if="error" class="error apollo">An error occured</div>
-
-                <div v-else-if="data" class="result apollo">
-
-                    <div class="question-box">
-                        <h3>{{ data.question.title }}</h3>
-                        <p>{{ data.question.text }}</p>
-                    </div>
-                    <br>
-
-                    <div
-                            v-for="answer of data.question.answerSet"
-                            :key="answer.id"
-                            class="answer-box"
-                    >
-                        A: {{ answer.text }} [{{ answer.votes }} Votes]
-                    </div>
-                </div>
-
-                <div v-else class="no-result apollo">No result :(</div>
-
-            </template>
-        </ApolloQuery>
+            A: {{ answer.text }} [{{ answer.votes }} Votes]
+        </div>
 
 
     </div>
@@ -41,14 +26,31 @@
 <script>
     export default {
         name: 'QuestionOverview',
-        /*props: {
-            question_id: Number
-        },*/
         data() {
             return {
+                question: {},
                 question_id: this.$route.params.id
             }
         },
+        watch: {
+            $route(to, from) { // eslint-disable-line no-unused-vars
+                this.question_id = to.params.id
+            }
+        },
+        apollo: {
+            question: {
+                // gql query
+                query: require('../graphql/QuestionDetail.gql'),
+                // Static parameters
+                variables() {
+                    return {
+                        question_id: this.question_id
+                    }
+                }
+            }
+        }
+
+
     }
 </script>
 
@@ -61,7 +63,6 @@
     .question-box {
         background-color: bisque;
     }
-
 
     .answer-box {
         background-color: antiquewhite;
