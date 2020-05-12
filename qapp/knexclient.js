9c1345b8050560454b222ada4e4dc213b0acf597
MrTrustworthy <tinywritingmakesmailhardtoread@gmail.com>
2020-05-12 20:51:22 +0200

    cleanup qapp folder structure

--- qapp/knexclient.js
+++
@@ -1,4 +0,0 @@
-const env = process.env.ENVIRONMENT || 'development';
-const knexconfig = require('./knexfile')[env];
-
-module.exports = require('knex')(knexconfig)
---
+++ qapp/src/knexclient.js
@@ -0,0 +1,4 @@
+const env = process.env.ENVIRONMENT || 'development';
+const knexconfig = require('../knexfile')[env];
+
+module.exports = require('knex')(knexconfig);
