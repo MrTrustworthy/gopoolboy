6386a1c558a51907c1caff780143bf7be5b9c993
MrTrustworthy <tinywritingmakesmailhardtoread@gmail.com>
2020-05-27 21:25:04 +0200

    bugfix and cleanup

--- applications/frontend/src/main.js
+++ applications/frontend/src/main.js
@@ -12,14 +12,6 @@ Vue.use(Auth0Plugin, {
     audience: process.env.VUE_APP_AUTH0_AUDIENCE,
 });
 
-console.log("Running with env VUE_APP_QAPP_GRAPHQL_HTTP", process.env.VUE_APP_QAPP_GRAPHQL_HTTP);
-console.log("Running with env VUE_APP_QAPP_GRAPHQL_TOKEN_NAME", process.env.VUE_APP_QAPP_GRAPHQL_TOKEN_NAME);
-console.log("Running with env VUE_APP_ORGAMON_GRAPHQL_HTTP", process.env.VUE_APP_ORGAMON_GRAPHQL_HTTP);
-console.log("Running with env VUE_APP_ORGAMON_GRAPHQL_TOKEN_NAME", process.env.VUE_APP_ORGAMON_GRAPHQL_TOKEN_NAME);
-console.log("Running with env VUE_APP_AUTH0_DOMAIN", process.env.VUE_APP_AUTH0_DOMAIN);
-console.log("Running with env VUE_APP_AUTH0_AUDIENCE", process.env.VUE_APP_AUTH0_AUDIENCE);
-console.log("Running with env VUE_APP_AUTH0_SPA_CLIENT_ID", process.env.VUE_APP_AUTH0_SPA_CLIENT_ID);
-
 new Vue({
     apolloProvider: createProvider(),
     router,
