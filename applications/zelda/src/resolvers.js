23d52713ad92c26fd254a9e20b088fed95fee508
MrTrustworthy <tinywritingmakesmailhardtoread@gmail.com>
2020-06-15 20:36:52 +0200

    resolve #131

--- applications/zelda/src/resolvers.js
+++ applications/zelda/src/resolvers.js
@@ -55,6 +55,8 @@ const getLinkedCrumbIds = async (args, organization, user) => {
 // Mutations
 
 const createCrumbLink = async (args, organization, user) => {
+    if (args.from === args.to) throw new Error("Can't link a crumb to itself");
+
     logger.info("Creating new crumb link", {to: args.toId, from: args.fromId, organization, user});
     let newIds = await knexClient("crumblinks")
         .insert({
